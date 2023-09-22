0da63f6fb0349a32f33259bbdbd884342e11904a
fibbbonachos <arish2k3@gmail.com>
2023-09-22 14:02:46 +0530

    update

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Fibz")
+	message, err := greetings.Hello("Fibz")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
