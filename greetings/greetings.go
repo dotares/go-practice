a20c749905e4ceb1fe1860cad88c423aad3b6f68
fibbbonachos <arish2k3@gmail.com>
2023-09-22 15:08:22 +0530

    update

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -11,7 +12,22 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	// If name exists
-	message := fmt.Sprintf("Hello, %v. Welcome to Go", name)
+	// Create message using random format
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
