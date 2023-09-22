982b86623469c7f1a9b81bd51a93645c25ed16e0
fibbbonachos <arish2k3@gmail.com>
2023-09-22 11:41:31 +0530

    update

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If name exists
 	message := fmt.Sprintf("Hello, %v. Welcome to Go", name)
-	return message 
+	return message, nil
 }
