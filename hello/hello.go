2e12b1d00f977214f90f4a42469830262bad3399
fibbbonachos <arish2k3@gmail.com>
2023-09-22 15:17:46 +0530

    update

--- hello/hello.go
+++ hello/hello.go
@@ -8,14 +8,9 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
-
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Get a greeting message and print it.
 	message, err := greetings.Hello("Fibz")
 
 	if err != nil {
